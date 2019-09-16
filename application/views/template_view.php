34908e9590bf32a8538a31a64778cd26beac541c
okki23 <okkisetyawan@gmail.com>
2019-09-16 22:42:29 +0700

    update list menu

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -141,55 +141,35 @@
                 
 				 
 					<ul class="list">
-					<!-- <li>
-                        <a href="<?php echo base_url('dashboard'); ?>">
-                            <i class="material-icons">home</i>
-                            <span>Home  </span>
+				 
+                    <li>
+                        <a href="<?php echo base_url('homepage'); ?>">
+                           <i class="material-icons">dns</i>
+                            <span>Homepage</span>
                         </a>
                     </li> 
-                    <li class="header">Menu Master</li> 
+                    
                     <li>
-                        <a href="<?php echo base_url('customer'); ?>">
+                        <a href="<?php echo base_url('profile'); ?>">
                            <i class="material-icons">dns</i>
-                            <span>Customer</span>
+                            <span>Profile</span>
                         </a>
-                    </li>  -->
+                    </li> 
+                    
                     <li>
                         <a href="<?php echo base_url('about'); ?>">
                            <i class="material-icons">dns</i>
                             <span>About</span>
                         </a>
                     </li> 
-                    <li>
-                        <a href="<?php echo base_url('cat_pricelist'); ?>">
-                           <i class="material-icons">dns</i>
-                            <span>Kategori Pricelist Item</span>
-                        </a>
-                    </li> 
-                    <li>
-                        <a href="<?php echo base_url('price_list_item'); ?>">
-                           <i class="material-icons">dns</i>
-                            <span>Pricelist Item</span>
-                        </a>
-                    </li> 
-                    <li>
-                        <a href="<?php echo base_url('price_list_package'); ?>">
-                           <i class="material-icons">dns</i>
-                            <span>Pricelist Package</span>
-                        </a>
-                    </li> 
+                  
                     <li>
                         <a href="<?php echo base_url('client'); ?>">
                            <i class="material-icons">dns</i>
                             <span>Client</span>
                         </a>
                     </li> 
-                     <li>
-                        <a href="<?php echo base_url('news'); ?>">
-                           <i class="material-icons">dns</i>
-                            <span>News</span>
-                        </a>
-                    </li> 
+                    
                     <li>
                         <a href="<?php echo base_url('googlemaps'); ?>">
                            <i class="material-icons">dns</i>
@@ -220,83 +200,14 @@
                             <span>Manajemen User</span>
                         </a>
                     </li> 
-                   <!--  <li>
-                        <a href="<?php echo base_url('user'); ?>">
-                           <i class="material-icons">dns</i>
-                            <span>Social Media</span>
-                        </a>
-                    </li>  -->
+                  
                     <li>
                         <a href="<?php echo base_url(); ?>" target="_blank">
                            <i class="material-icons">dns</i>
                             <span>Pratinjau Website</span>
                         </a>
                     </li> 
-                    <!-- <li>
-                        <a href="<?php echo base_url('sales'); ?>">
-                           <i class="material-icons">dns</i>
-                            <span>Sales</span>
-                        </a>
-                    </li>
-                     <li>
-                        <a href="<?php echo base_url('admin_pppu'); ?>">
-                           <i class="material-icons">dns</i>
-                            <span>Admin PPPU</span>
-                        </a>
-                    </li> 
-                    <li>
-                        <a href="javascript:void(0);" class="menu-toggle">
-                            <i class="material-icons">dns</i>
-                            <span>User</span>
-                        </a>
-                        <ul class="ml-menu">
-                            <li>
-                                <a href="<?php echo base_url('akun_sales'); ?>">Akun Sales</a>
-                            </li>
-                            <li>
-                                 <a href="<?php echo base_url('akun_admin_pppu'); ?>">Akun Admin PPPU</a>
-                            </li> 
-                        </ul>
-                    </li>
-                    <li class="header">Menu Transaksi</li>
-                    <li>
-                        <a href="<?php echo base_url('booking_fee'); ?>">
-                            <i class="material-icons">dns</i>
-                            <span>Booking Fee</span>
-                        </a>
-                    </li> 
-                    <li>
-                        <a href="<?php echo base_url('pu'); ?>">
-                            <i class="material-icons">dns</i>
-                            <span>PU</span>
-                        </a>
-                    </li> 
-                    <li>
-                        <a href="<?php echo base_url('validasi_pppu'); ?>">
-                            <i class="material-icons">dns</i>
-                            <span>Validasi P3U</span>
-                        </a>
-                    </li> 
-                      <li>
-                        <a href="<?php echo base_url('booking_fee'); ?>">
-                            <i class="material-icons">dns</i>
-                            <span>Refund</span>
-                        </a>
-                    </li> 
-                    <li>
-					<li class="header">Laporan</li>
-                    <li>
-                        <a href="<?php echo base_url('closing'); ?>">
-                            <i class="material-icons">dns</i>
-                            <span>Report Closing</span>
-                        </a>
-                    </li> 
-                    <li>
-                        <a href="<?php echo base_url('refund'); ?>">
-                            <i class="material-icons">dns</i>
-                            <span>Report Refund</span>
-                        </a>
-                    </li>   -->
+                    
 					</ul>
 					
 				  
